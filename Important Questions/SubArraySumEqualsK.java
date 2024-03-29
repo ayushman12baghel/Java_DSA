7b0dd5e5dba81f319b18d53dc3499909e95328eb
ayushman12baghel <ayushman12baghel@gmail.com>
2024-03-29 21:45:59 +0530

    Subarray Sum equals k

---
+++ Important Questions/SubArraySumEqualsK.java
@@ -0,0 +1,30 @@
+public class SubArraySumEqualsK {
+    public static int SubarraySumEqualsK(int arr[], int k) {
+        int count = 0;
+        int currSum = 0;
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if (i == 0) {
+                    currSum = prefix[j];
+                } else {
+                    currSum = prefix[j] - prefix[i - 1];
+                }
+                if (currSum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3 };
+        int k = 3;
+        System.out.println(SubarraySumEqualsK(arr, k));
+    }
+}
