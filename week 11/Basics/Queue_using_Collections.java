aacd6ceaa91e7a2fcc4ab64f5cdafefcd0bffc94
ayushman12baghel <ayushman12baghel@gmail.com>
2024-04-11 22:40:48 +0530

    Queue Using Collections

---
+++ week 11/Basics/Queue_using_Collections.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Queue_using_Collections {
+    public static void main(String args[]) {
+        // Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
