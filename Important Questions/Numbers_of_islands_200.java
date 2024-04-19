59666f6471641587353cc4d6fc07facf3e94af8e
ayushman12baghel <ayushman12baghel@gmail.com>
2024-04-19 13:54:25 +0530

    Number of Islands leetcode 200

---
+++ Important Questions/Numbers_of_islands_200.java
@@ -0,0 +1,34 @@
+public class Numbers_of_islands_200 {
+    public static void dfs(char grid[][], int i, int j) {
+        if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == '1') {
+            grid[i][j] = '0';
+            dfs(grid, i + 1, j);
+            dfs(grid, i - 1, j);
+            dfs(grid, i, j + 1);
+            dfs(grid, i, j - 1);
+        }
+    }
+
+    public static int numIslands(char grid[][]) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String args[]) {
+        char grid[][] = {
+                { '1', '1', '0', '0', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '1', '0', '0' },
+                { '0', '0', '0', '1', '1' }
+        };
+        System.out.println(numIslands(grid));
+    }
+}
