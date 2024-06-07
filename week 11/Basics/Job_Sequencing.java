2bd8355f113af2eca4b60309584e8c1007a70fb1
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-08 04:33:38 +0530

    Job Sequence

---
+++ week 11/Basics/Job_Sequencing.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Job_Sequencing {
+
+    static class Job {
+        int deadline;
+        int profit;
+        int id;
+
+        public Job(int i, int d, int p) {
+            id = i;
+            deadline = d;
+            profit = p;
+        }
+    }
+
+    public static void main(String[] args) {
+        int jobInfo[][] = { { 4, 20 },
+                { 1, 10 }, { 1, 40 }, { 1, 30 } };
+
+        ArrayList<Job> jobs = new ArrayList<>();
+
+        for (int i = 0; i < jobInfo.length; i++) {
+            jobs.add(new Job(i, jobInfo[i][0], jobInfo[i][1]));
+        }
+
+        Collections.sort(jobs, (obj1, obj2) -> obj2.profit - obj1.profit); // descending order of profit
+
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for (int i = 0; i < jobs.size(); i++) {
+            Job curr = jobs.get(i);
+            if (curr.deadline > time) {
+                seq.add(curr.id);
+                time++;
+            }
+        }
+
+        // print seq
+        System.out.println("max jobs: " + seq.size());
+        for (int i = 0; i < seq.size(); i++) {
+            System.out.print(seq.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
