e0a6fa6a2a357e901863fa3db906cefde79f81d1
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-09 16:14:27 +0530

    Postorder Traversal

---
+++ week 12/Basics/Postorder_Traversal.java
@@ -0,0 +1,48 @@
+public class Postorder_Traversal {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public static void postorder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        tree.postorder(root);
+    }
+}
