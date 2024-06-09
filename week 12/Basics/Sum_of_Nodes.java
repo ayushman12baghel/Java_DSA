139f211e4fedccdaf47babf25f2703894aad0d55
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-09 21:49:58 +0530

    Sum of Nodes

---
+++ week 12/Basics/Sum_of_Nodes.java
@@ -0,0 +1,47 @@
+public class Sum_of_Nodes {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static int sumOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+        return root.data + leftSum + rightSum;
+    }
+
+    public static void main(String args[]) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(sumOfNodes(root));
+    }
+}
---
+++ week 12/Practice/SumOfNodes.java
@@ -0,0 +1,41 @@
+public class SumOfNodes {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int SumNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lsum = SumNodes(root.left);
+        int rsum = SumNodes(root.right);
+        return root.data + lsum + rsum;
+    }
+
+    public static void main(String args[]) {
+        /*
+         * 1
+         * / \
+         * 2 3
+         * / \ / \
+         * 4 5 6 7
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(SumNodes(root));
+    }
+}
