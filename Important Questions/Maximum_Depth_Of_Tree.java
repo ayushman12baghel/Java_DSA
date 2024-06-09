deede95bbea858e2bc9953123dc3fe3602871a5a
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-10 02:28:17 +0530

    Maximum Depth of Binary Tree 104 Leetcode

---
+++ Important Questions/Maximum_Depth_Of_Tree.java
@@ -0,0 +1,48 @@
+public class Maximum_Depth_Of_Tree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static void main(String args[]) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(maxDepth(root));
+    }
+}
