fa9b087e2b1fcbf43e6664e4012c473ed261a0a6
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-10 02:34:26 +0530

    Count Complete Tree Nodes 222 Leetcode

---
+++ Important Questions/Count_Complete_Tree_Nodes.java
@@ -0,0 +1,49 @@
+public class Count_Complete_Tree_Nodes {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lc = countNodes(root.left);
+        int rc = countNodes(root.right);
+
+        return lc + rc + 1;
+    }
+
+    public static void main(String args[]) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(countNodes(root));
+    }
+}
