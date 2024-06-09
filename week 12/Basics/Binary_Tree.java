7782c6267ffbc7dea032294a3d7cadcbb5458ef2
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-09 15:13:09 +0530

    Preorder Tree

---
+++ week 12/Basics/Binary_Tree.java
@@ -0,0 +1,38 @@
+public class Binary_Tree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.left = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+    }
+}
