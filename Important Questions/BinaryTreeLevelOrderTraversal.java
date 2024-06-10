7b586458ed5a6dfaeab22cf9476312379ad36f36
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-11 00:51:20 +0530

    Binary Tree Levelorder Traversal 102 Leetcode

---
+++ Important Questions/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> currLevel = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = q.poll();
+                currLevel.add(currNode.val);
+                if (currNode.left != null)
+                    q.add(currNode.left);
+                if (currNode.right != null)
+                    q.add(currNode.right);
+            }
+            res.add(currLevel);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // Create the binary tree [3,9,20,null,null,15,7]
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        List<List<Integer>> result = levelOrder(root);
+
+        // Print the result
+        for (List<Integer> level : result) {
+            System.out.println(level);
+        }
+    }
+}
