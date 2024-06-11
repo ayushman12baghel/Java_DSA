fa4e7aa18ca50c2d5388d196d24bc06150f53370
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-12 02:48:46 +0530

    Minimum Distance Between Nodes

---
+++ week 12/Practice/MinimumDistanceBetweenNodes.java
@@ -0,0 +1,83 @@
+public class MinimumDistanceBetweenNodes {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node left = lca(root.left, n1, n2);
+        Node right = lca(root.right, n1, n2);
+
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+
+        return root;
+    }
+
+    public static int lcaDistance(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.data == n) {
+            return 0;
+        }
+
+        int left = lcaDistance(root.left, n);
+        int right = lcaDistance(root.right, n);
+
+        if (left == -1 && right == -1) {
+            return -1;
+        } else if (left == -1) {
+            return right + 1;
+        } else {
+            return left + 1;
+        }
+    }
+
+    public static int minDistance(Node root, int n1, int n2) {
+        Node lca = lca(root, n1, n2);
+        int dist1 = lcaDistance(lca, n1);
+        int dist2 = lcaDistance(lca, n2);
+
+        return dist1 + dist2;
+    }
+
+    public static void main(String args[]) {
+        /*
+         * 1
+         * / \
+         * 2 3
+         * / \ / \
+         * 4 5 6 7
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1 = 4, n2 = 6;
+        System.out.println(minDistance(root, n1, n2));
+    }
+}
