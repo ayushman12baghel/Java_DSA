f8f502e08e6810f6c59994d2af8af430adb38092
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-12 02:08:36 +0530

    Lowest Common Ancestor 236 Leetcode

---
+++ Important Questions/LowestCommonAncestor.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class LowestCommonAncestor {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node left = lca(root.left, n1, n2);
+        Node right = lca(root.right, n1, n2);
+
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+
+    public static void main(String args[]) {
+        /*
+         * 1
+         * / \
+         * 2 3
+         * / \ / \
+         * 4 5 6 7
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1 = 4, n2 = 7;
+
+        System.out.println(lca(root, n1, n2).data);
+    }
+}
