080d2ddb42e935c2953b882c69345571b1277335
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-14 22:28:01 +0530

    Build BST Tree

---
+++ week 13/practice/BuildBSTtree.java
@@ -0,0 +1,48 @@
+public class BuildBSTtree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String args[]) {
+        int values[] = { 10, 7, 6, 8, 5, 2, 1, 3, 4, 9 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+    }
+}
