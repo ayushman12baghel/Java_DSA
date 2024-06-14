98e5edb9eb62128c907481345da95f352c59c758
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-14 22:56:04 +0530

    Search in BST Tree

---
+++ week 13/Basics/Search_in_BST.java
@@ -0,0 +1,70 @@
+public class Search_in_BST {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (root.data > key) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    public static void main(String args[]) {
+        int values[] = { 8, 5, 10, 3, 6, 1, 4, 11, 14 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+
+        if (search(root, 1)) {
+            System.out.println("Found");
+        } else {
+            System.out.println("Not found");
+        }
+    }
+
+}
