9bef1b9e1df304be66330cae51e6c6685d1bf052
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-16 00:58:20 +0530

    Sorted Array to Balanced BST

---
+++ week 13/Basics/Sorted_Array_to_Balanced_BST.java
@@ -0,0 +1,50 @@
+public class Sorted_Array_to_Balanced_BST {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static Node createBST(int arr[], int st, int end) {
+        if (st > end) {
+            return null;
+        }
+
+        int mid = (st + end) / 2;
+        Node root = new Node(arr[mid]);
+        root.left = createBST(arr, st, mid - 1);
+        root.right = createBST(arr, mid + 1, end);
+
+        return root;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 3, 5, 6, 8, 10, 11, 12 };
+        /*
+         * 8
+         * / \
+         * 5 11
+         * / \ / \
+         * 3 6 10 12
+         * expected BST
+         */
+        Node root = createBST(arr, 0, arr.length - 1);
+        preorder(root);
+    }
+}
