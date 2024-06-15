6bf0f54314d971f98b08471293125f32559ce69b
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-15 22:30:15 +0530

    Validate BST

--- week 13/Basics/Validate_BST.java
+++ week 13/Basics/Validate_BST.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
+import javax.swing.tree.TreeNode;
+
+import org.w3c.dom.Node;
+
 public class Validate_BST {
 
     static class Node {
@@ -51,6 +55,31 @@ public class Validate_BST {
         return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
     }
 
+    public static void inorder2(Node root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder2(root.left, list);
+        list.add(root.data);
+        inorder2(root.right, list);
+
+    }
+
+    public static boolean validate(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) >= list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidBST2(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        inorder2(root, list);
+        return validate(list);
+    }
+
     public static void main(String args[]) {
         int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
@@ -61,6 +90,11 @@ public class Validate_BST {
         System.out.println();
 
         if (isValidBST(root, null, null)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("not Valid.0");
+        }
+        if (isValidBST2(root)) {
             System.out.println("Valid");
         } else {
             System.out.println("not Valid.0");
