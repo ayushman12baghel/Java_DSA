69b7ec6e9cd1db58ce7655786bacf9fe0949d53b
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-16 00:13:36 +0530

    Mirror a BST

---
+++ week 13/Basics/Mirror_a_BST.java
@@ -0,0 +1,71 @@
+public class Mirror_a_BST {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static Node mirror(Node root) {
+        if (root == null) {
+            return root;
+        }
+
+        Node left = mirror(root.left);
+        Node right = mirror(root.right);
+
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+
+    public static void main(String args[]) {
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+
+        preorder(root);
+    }
+}
