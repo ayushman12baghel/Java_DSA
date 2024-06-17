4976630559a24a87085bbca47950f3e6c1e61f10
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-18 00:27:21 +0530

    Merge Binary Trees 617 Leetcode

---
+++ Important Questions/MergeBinaryTrees.java
@@ -0,0 +1,39 @@
+
+public class MergeBinaryTrees {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(3);
+        root1.right = new TreeNode(5);
+
+        TreeNode root2 = new TreeNode(2);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(4);
+
+        // Merge the trees
+        TreeNode mergedTree = mergeTrees(root1, root2);
+    }
+}
