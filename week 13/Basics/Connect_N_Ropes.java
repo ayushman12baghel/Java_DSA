eece85aa4c1b1b7e10fc6c36671cb1d3a01e64fb
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-21 03:19:35 +0530

    Connect N ropes

---
+++ week 13/Basics/Connect_N_Ropes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Connect_N_Ropes {
+    public static void main(String args[]) {
+        int ropes[] = { 4, 3, 2, 6 };
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < ropes.length; i++) {
+            pq.add(ropes[i]);
+        }
+
+        int cost = 0;
+        while (pq.size() > 1) {
+            int min = pq.remove();
+            int min2 = pq.remove();
+            cost += min + min2;
+            pq.add(min + min2);
+        }
+
+        System.out.println(cost);
+    }
+}
