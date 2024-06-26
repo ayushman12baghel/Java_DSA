d006157d3ff40e1f56c5122f81e40a98e4cb8f1e
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-27 02:41:24 +0530

    TreeMap

---
+++ week 14/Basics/TreeMap_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TreeMap_ {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> lhm = new TreeMap<>();
+        lhm.put("India", 100);
+        lhm.put("China", 150);
+        lhm.put("US", 50);
+        lhm.put("Nepal", 10);
+        System.out.println(lhm);
+    }
+}
