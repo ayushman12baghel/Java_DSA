1542f1f1a2f53fc2d5a06c58e525175174c0a2c5
ayushman12baghel <ayushman12baghel@gmail.com>
2024-06-27 20:13:09 +0530

    TreeSet

---
+++ week 14/Basics/TreeSet_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TreeSet_ {
+    public static void main(String args[]) {
+        TreeSet<String> cities = new TreeSet<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Bhopal");
+        cities.add("Indore");
+        System.out.println(cities);// sorted order
+    }
+}
