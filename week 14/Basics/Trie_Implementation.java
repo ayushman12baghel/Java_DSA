afa56090583b766091637bd341cd7c084174f7d1
ayushman12baghel <ayushman12baghel@gmail.com>
2024-07-06 23:43:44 +0530

    Trie Implementation

--- week 14/Basics/Trie_Implementation.java
+++ week 14/Basics/Trie_Implementation.java
@@ -13,8 +13,40 @@ public class Trie_Implementation {
 
     public static Node root = new Node();
 
-    public static void main(String[] args) {
-        String words = { "the", "a", "there", "their", "thee" };
+    public static void insert(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
 
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return curr.eow == true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "the", "a", "there", "their", "thee" };
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+
+        System.out.println(search("thee"));
+        System.out.println(search("thor"));
     }
 }
