0e3b7759be71720b0ea22b22666e4633ec96a844
ayushman12baghel <ayushman12baghel@gmail.com>
2024-07-09 17:41:18 +0530

    Count Unique Substrings

---
+++ week 14/Basics/Count_Unique_Prefix.java
@@ -0,0 +1,69 @@
+import org.w3c.dom.Node;
+
+public class Count_Unique_Prefix {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return curr.eow == true;
+    }
+
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null) {
+                count += countNodes(root.children[i]);
+            }
+        }
+
+        return count + 1;
+    }
+
+    public static void main(String[] args) {
+        String str = "ababa";// ans=10
+
+        // suffix -> insert in trie
+        for (int i = 0; i < str.length(); i++) {
+            String suffix = str.substring(i);
+            insert(suffix);
+        }
+
+        System.out.println(countNodes(root));
+    }
+}
