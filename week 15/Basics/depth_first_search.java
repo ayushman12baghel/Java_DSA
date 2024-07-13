7f8ab12fbaf2452f766a658486e6d0627a8e3c40
ayushman12baghel <ayushman12baghel@gmail.com>
2024-07-13 21:40:52 +0530

    Depth First Search

---
+++ week 15/Basics/depth_first_search.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+public class depth_first_search {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.wt = w;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {// O(V+E)
+
+        for (int i = 0; i < 7; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0-vertex
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        // 1-vertex
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        // 2-vertex
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        // 3-vertex
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        // 4-vertex
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        // 5-vertex
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        // 6-vertex
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph, int curr, boolean vis[]) {
+        // visit
+        System.out.print(curr + " ");
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfs(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int V = 7;
+        // int arr[]=new arr[V];
+        ArrayList<Edge>[] graph = new ArrayList[V];// null -> empty arraylist
+
+        createGraph(graph);
+        dfs(graph, 0, new boolean[V]);
+    }
+}
