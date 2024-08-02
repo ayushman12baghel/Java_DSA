4823f23b1d21ad2e227fd490ae8e94c181478f59
ayushman12baghel <ayushman12baghel@gmail.com>
2024-08-02 23:21:17 +0530

    Flood Fill Algorithm

---
+++ week 15/Basics/Flood_fill_Algorithm.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Flood_fill_Algorithm {
+    public void helper(int[][] image, int sr, int sc, int color, boolean vis[], int orgColor) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || vis[sr][sc]
+                || image[sr][sc] != orgColor) {
+            return;
+        }
+
+        // left
+        helper(image, sr, sc - 1, color, vis, orgColor);
+
+        // right
+        helper(image, sr, sc + 1, color, vis, orgColor);
+
+        // up
+        helper(image, sr - 1, sc, color, vis, orgColor);
+
+        // down
+        helper(image, sr + 1, sc, color, vis, orgColor);
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[] = new boolean[image.length][image[0].length];
+        helper(image, sr, sc, color, vis, image[sr][sc]);
+        return image;
+    }
+
+}
