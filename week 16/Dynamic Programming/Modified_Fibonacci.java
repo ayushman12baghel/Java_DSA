03be2172b0f53a9a38dfc7e037dbbb3fa8d27be8
ayushman12baghel <ayushman12baghel@gmail.com>
2024-08-12 00:38:39 +0530

    Fibonacci Dynamic Programming

--- week 16/Dynamic Programming/Modified_Fibonacci.java
+++ week 16/Dynamic Programming/Modified_Fibonacci.java
@@ -10,9 +10,22 @@ public class Modified_Fibonacci {
         return fib[n];
     }
 
+    public static int fib(int n) {
+        int dp[] = new int[n + 1];
+
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
         int f[] = new int[n + 1];
         System.out.println(fibonacci(n, f));
+        System.out.println(fib(n));
     }
 }
