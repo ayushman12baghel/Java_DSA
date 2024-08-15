15a31c09fa6bdac002bb70dc32d14a29cc30824c
ayushman12baghel <ayushman12baghel@gmail.com>
2024-08-15 12:46:57 +0530

    Longest Common Substring

---
+++ week 16/Dynamic Programming/Longest_Common_Substring.java
@@ -0,0 +1,36 @@
+public class Longest_Common_Substring {
+    public static int longestCommonSubstring(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n + 1][m + 1];
+        int ans = 0;
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (j == 0 || i == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcdgh";
+        String str2 = "acdghr";
+
+        System.out.println(longestCommonSubstring(str1, str2));
+    }
+}
