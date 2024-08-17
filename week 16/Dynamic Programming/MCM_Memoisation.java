b0b048bf420c140f70beca2a3e34b07ceb207598
ayushman12baghel <ayushman12baghel@gmail.com>
2024-08-18 00:10:42 +0530

    MCM by Memoization

---
+++ week 16/Dynamic Programming/MCM_Memoisation.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class MCM_Memoisation {
+    public static int mcm(int arr[], int i, int j, int dp[][]) {
+        if (i == j) {
+            return 0;// single matrix case
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcm(arr, i, k, dp);
+            int cost2 = mcm(arr, k + 1, j, dp);
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+        }
+
+        return dp[i][j] = ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 3 };
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(mcm(arr, 1, n - 1, dp));
+    }
+}
