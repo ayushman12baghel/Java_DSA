dbacc51986026ef8d124c5e76d8246cbf066b1c8
ayushman12baghel <ayushman12baghel@gmail.com>
2024-08-17 13:38:32 +0530

    Count number of possible BST

---
+++ week 16/Dynamic Programming/Counting_Bst_Trees.java
@@ -0,0 +1,20 @@
+public class Counting_Bst_Trees {
+    public static int countBST(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+
+        System.out.println(countBST(n));
+    }
+}
