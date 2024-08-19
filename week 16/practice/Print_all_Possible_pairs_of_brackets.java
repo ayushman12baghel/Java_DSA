2ac556349fb9821a3e351dc2ecbe0d5311a8788d
ayushman12baghel <ayushman12baghel@gmail.com>
2024-08-19 22:35:57 +0530

    Print all Possible valid Parentheses

--- week 16/practice/Print_all_Possible_pairs_of_brackets.java
+++ week 16/practice/Print_all_Possible_pairs_of_brackets.java
@@ -1,10 +1,12 @@
 public class Print_all_Possible_pairs_of_brackets {
+    static int count = 0;
+
     public static void printParenthesisUtil(char str[], int pos, int n, int open, int close) {
         if (close == n) {
             for (int i = 0; i < str.length; i++) {
                 System.out.print(str[i]);
             }
-            System.out.println();
+            System.out.println(++count);
             return;
         } else {
             if (open > close) {
@@ -25,8 +27,8 @@ public class Print_all_Possible_pairs_of_brackets {
     }
 
     public static void main(String args[]) {
-        int n = 3;
+        int n = 4;
         char str[] = new char[2 * n];
-        printParenthesis(2, str);
+        printParenthesis(n, str);
     }
 }
