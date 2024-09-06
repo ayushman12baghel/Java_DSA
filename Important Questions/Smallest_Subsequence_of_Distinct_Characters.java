a8199bd76f93e38470606c56c0b9442b0534e958
ayushman12baghel <ayushman12baghel@gmail.com>
2024-09-06 23:45:21 +0530

    Smallest Subsequence of Distinct Chars Leetcode 1081

---
+++ Important Questions/Smallest_Subsequence_of_Distinct_Characters.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Smallest_Subsequence_of_Distinct_Characters {
+
+    public static String smallestSubsequence(String str) {
+        int lastOccurence[] = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            lastOccurence[str.charAt(i) - 'a'] = i;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        boolean taken[] = new boolean[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (taken[ch - 'a']) {
+                continue;
+            }
+
+            while (!stack.isEmpty() && stack.peek() > ch && i < lastOccurence[stack.peek() - 'a']) {
+                taken[stack.pop() - 'a'] = false;
+            }
+
+            stack.push(ch);
+            taken[ch - 'a'] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        sb.reverse();
+
+        return sb.toString();
+    }
+
+    public static void main(String args[]) {
+        String str = "cbacdcbc";
+        System.out.println(smallestSubsequence(str));
+    }
+}
