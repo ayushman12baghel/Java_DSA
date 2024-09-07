4940f5c90bffe83c61e7cbb02c04a7a02eb05b2b
ayushman12baghel <ayushman12baghel@gmail.com>
2024-09-07 23:22:49 +0530

    Longest Substring without Repeating Characters Leetcode 3

--- Important Questions/Longest_Substring_without_Repeating_Characters.java
+++ Important Questions/Longest_Substring_without_Repeating_Characters.java
@@ -24,9 +24,30 @@ public class Longest_Substring_without_Repeating_Characters {
         return length;
     }
 
+    public static int longestSubstringWithoutRepeating2(String str) {
+        int start = 0;
+        int end = 0;
+        int maxLen = 0;
+        List<Character> list = new ArrayList<>();
+
+        while (end < str.length()) {
+            if (!list.contains(str.charAt(end))) {
+                list.add(str.charAt(end));
+                end++;
+                maxLen = Math.max(list.size(), maxLen);
+            } else {
+                list.remove(Character.valueOf(str.charAt(start)));
+                start++;
+            }
+        }
+
+        return maxLen;
+    }
+
     public static void main(String[] args) {
         String str = "abcabcbb";
 
         System.out.println(longestSubstringWithoutRepeating(str));
+        System.out.println(longestSubstringWithoutRepeating2(str));
     }
 }
