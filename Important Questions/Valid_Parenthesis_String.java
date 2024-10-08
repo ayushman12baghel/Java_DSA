ed71352b68f8d2e305289e42e659d7a4226fdc3b
ayushman12baghel <ayushman12baghel@gmail.com>
2024-10-08 21:14:37 +0530

    Valid Parenthesis String Leetcode 678

--- Important Questions/Valid_Parenthesis_String.java
+++ Important Questions/Valid_Parenthesis_String.java
@@ -32,9 +32,44 @@ public class Valid_Parenthesis_String {
         return minOpen == 0;
     }
 
+    public static boolean checkValidString2(String str) {
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> aster = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(') {
+                open.push(i);
+            } else if (c == '*') {
+                aster.push(i);
+            } else {
+                if (!open.isEmpty()) {
+                    open.pop();
+                } else if (!aster.isEmpty()) {
+                    aster.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!open.isEmpty() && !aster.isEmpty()) {
+            if (open.peek() < aster.peek()) {
+                open.pop();
+                aster.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return open.isEmpty();
+    }
+
     public static void main(String args[]) {
         String str = "(*))";
 
+        System.out.println(checkValidString(str));
         System.out.println(checkValidString(str));
     }
 }
