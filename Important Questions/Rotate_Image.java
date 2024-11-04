7f54d0bad0d2a9f5ea8bea8ed78dd390b436cda9
ayushman12baghel <ayushman12baghel@gmail.com>
2024-11-04 23:50:43 +0530

    Rotate Image Leetcode 48

---
+++ Important Questions/Rotate_Image.java
@@ -0,0 +1,42 @@
+public class Rotate_Image {
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            reverseRow(matrix[i]);
+        }
+    }
+
+    public static void reverseRow(int[] row) {
+        int left = 0;
+        int right = row.length - 1;
+
+        while (left < right) {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        rotate(matrix);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
