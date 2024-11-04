5f7b8d626f2a58a19b288574d06b78de6628e755
ayushman12baghel <ayushman12baghel@gmail.com>
2024-11-04 23:43:40 +0530

    Transpose Matrix Leetcode 867

---
+++ Important Questions/Transpose_Matrix.java
@@ -0,0 +1,31 @@
+public class Transpose_Matrix {
+    public static void print(int matrix[][]) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] transpose(int matrix[][]) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int transpose[][] = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        return transpose;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        print(matrix);
+        System.out.println();
+        int transpose[][] = transpose(matrix);
+        print(transpose);
+    }
+}
