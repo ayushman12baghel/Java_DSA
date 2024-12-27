1f1f5960c58893849dd2c2a4f1bf4d240f4a4321
ayushman12baghel <ayushman12baghel@gmail.com>
2024-12-27 13:34:15 +0530

    1014. Best Sightseeing Pair -> Leetcode

---
+++ Important Questions/Best_Sighseeing_Pair.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Best_Sighseeing_Pair {
+
+    public static int maxScoreSightseeingPair(int values[]) {
+        int max = Integer.MIN_VALUE;
+        int previous[] = new int[values.length];
+        previous[0] = values[0];
+
+        for (int i = 1; i < values.length; i++) {
+            previous[i] = Math.max(previous[i - 1], values[i] + i);
+        }
+
+        for (int i = 1; i < values.length; i++) {
+            max = Math.max(max, values[i] - i + previous[i - 1]);
+        }
+
+        return max;
+    }
+
+    public static void main(String args[]) {
+        int values[] = { 8, 1, 5, 2, 6 };
+        System.out.println(maxScoreSightseeingPair(values));
+    }
+}
