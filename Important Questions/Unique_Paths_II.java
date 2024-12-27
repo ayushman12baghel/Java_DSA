e6454dcbb311d81effdb5deae8c55e755281f245
ayushman12baghel <ayushman12baghel@gmail.com>
2024-12-27 16:13:57 +0530

    63. Unique Paths II -> Leetcode

--- Important Questions/Unique_Paths_II.java
+++ Important Questions/Unique_Paths_II.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Unique_Paths_II {
 
+    // Memoisation
     public static int uniquePaths(int obstacleGrid[][]) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
@@ -33,8 +34,49 @@ public class Unique_Paths_II {
         return dp[row][col];
     }
 
+    // Tabulation
+    public static int uniquePaths2(int obstacleGrid[][]) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int dp[][] = new int[m][n];
+
+        for (int col = 0; col < n; col++) {
+            if (col > 0 && obstacleGrid[0][col - 1] == 1) {
+                obstacleGrid[0][col] = 1;
+                dp[0][col] = 0;
+            } else if (obstacleGrid[0][col] == 1) {
+                dp[0][col] = 0;
+            } else {
+                dp[0][col] = 1;
+            }
+        }
+        for (int row = 0; row < m; row++) {
+            if (row > 0 && obstacleGrid[row - 1][0] == 1) {
+                obstacleGrid[row][0] = 1;
+                dp[row][0] = 0;
+            } else if (obstacleGrid[row][0] == 1) {
+                dp[row][0] = 0;
+            } else {
+                dp[row][0] = 1;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
     public static void main(String args[]) {
         int obstacleGrid[][] = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
         System.out.println(uniquePaths(obstacleGrid));
+        System.out.println(uniquePaths2(obstacleGrid));
     }
 }
