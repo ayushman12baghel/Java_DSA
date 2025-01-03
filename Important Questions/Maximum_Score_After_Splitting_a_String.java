0c4497b86f408263f7eaa7a91606e858431a7ba7
ayushman12baghel <ayushman12baghel@gmail.com>
2025-01-03 12:41:38 +0530

    1422. Maximum Score After Splitting a String

--- Important Questions/Maximum_Score_After_Splitting_a_String.java
+++ Important Questions/Maximum_Score_After_Splitting_a_String.java
@@ -1,6 +1,6 @@
 public class Maximum_Score_After_Splitting_a_String {
 
-    public int maxScore(String s) {
+    public static int maxScore(String s) {
         int result = 0;
         for (int i = 0; i < s.length() - 1; i++) {
             int curr = 0;
@@ -21,7 +21,56 @@ public class Maximum_Score_After_Splitting_a_String {
         return result;
     }
 
-    public static void main(String args[]) {
+    public static int maxScore2(String s) {
+        int ans = 0;
+        int ones = 0;
 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                ones++;
+            }
+        }
+
+        int zeros = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '1') {
+                ones--;
+            } else {
+                zeros++;
+            }
+
+            ans = Math.max(ones + zeros, ans);
+        }
+
+        return ans;
+    }
+
+    public static int maxScore3(String s) {
+        int ans = Integer.MIN_VALUE;
+        int ones = 0;
+        int zeros = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+
+            ans = Math.max(zeros - ones, ans);
+        }
+        if (s.charAt(s.length() - 1) == '1') {
+            ones++;
+        }
+
+        return ans + ones;
+    }
+
+    public static void main(String args[]) {
+        String str = "011101";
+        System.out.println(maxScore2(str));
+        System.out.println(maxScore(str));
+        System.out.println(maxScore3(str));
     }
 }
