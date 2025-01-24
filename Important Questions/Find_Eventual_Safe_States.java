6e519a93092c2aa5ef0c29cdee670154207ee855
ayushman12baghel <ayushman12baghel@gmail.com>
2025-01-24 22:11:20 +0530

    Added Second Soln using Kahn's Algo

--- Important Questions/Find_Eventual_Safe_States.java
+++ Important Questions/Find_Eventual_Safe_States.java
@@ -1,7 +1,9 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class Find_Eventual_Safe_States {
 
+    // DFS
     public static List<Integer> eventualSafeStates(int graph[][]) {
         boolean visited[] = new boolean[graph.length];
         boolean safe[] = new boolean[graph.length];
@@ -38,9 +40,59 @@ public class Find_Eventual_Safe_States {
         return false;
     }
 
+    // Using BFS Kahn's Algorithm
+    public static List<Integer> eventualSafeNodes2(int[][] graph) {
+        int n = graph.length;
+        int indeg[] = new int[n];
+        boolean safe[] = new boolean[n];
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int node : graph[i]) {
+                list.get(node).add(i);
+                indeg[i]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            safe[current] = true;
+
+            for (int neighbour : list.get(current)) {
+                indeg[neighbour]--;
+                if (indeg[neighbour] == 0) {
+                    queue.offer(neighbour);
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (safe[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String args[]) {
         int graph[][] = { { 1, 2 }, { 2, 3 }, { 5 }, { 0 }, { 5 }, {}, {} };
 
         System.out.println(eventualSafeStates(graph));
+        System.out.println(eventualSafeNodes2(graph));
     }
 }
