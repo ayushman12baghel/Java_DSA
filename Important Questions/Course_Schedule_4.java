4f96361977dd4dfe51d4f55dab23a7669c0dc69d
ayushman12baghel <ayushman12baghel@gmail.com>
2025-01-26 14:53:32 +0530

    1462. Course Schedule IV

--- Important Questions/Course_Schedule_4.java
+++ Important Questions/Course_Schedule_4.java
@@ -49,6 +49,54 @@ public class Course_Schedule_4 {
     // Topological Sort - Kahn's Algorithm
     public static List<Boolean> checkIfPrerequisite2(int numCourses, int[][] prerequisites, int[][] queries) {
         List<List<Integer>> list = new ArrayList<>();
+        boolean canReach[][] = new boolean[numCourses][numCourses];
+        int indeg[] = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int pre[] : prerequisites) {
+            list.get(pre[0]).add(pre[1]);
+            indeg[pre[1]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (int neighbour : list.get(current)) {
+                indeg[neighbour]--;
+                if (indeg[neighbour] == 0) {
+                    queue.offer(neighbour);
+                }
+                canReach[current][neighbour] = true;
+
+                for (int i = 0; i < numCourses; i++) {
+                    if (canReach[i][current]) {
+                        canReach[i][neighbour] = true;
+                    }
+                }
+            }
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+        for (int query[] : queries) {
+            ans.add(canReach[query[0]][query[1]]);
+        }
+
+        return ans;
+    }
+
+    public static List<Boolean> checkIfPrerequisite3(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<List<Integer>> list = new ArrayList<>();
         int indeg[] = new int[numCourses];
 
         for (int i = 0; i < numCourses; i++) {
@@ -99,5 +147,6 @@ public class Course_Schedule_4 {
 
         System.out.println(checkIfPrerequisite(numCourses, prerequisites, queries));
         System.out.println(checkIfPrerequisite2(numCourses, prerequisites, queries));
+        System.out.println(checkIfPrerequisite3(numCourses, prerequisites, queries));
     }
 }
