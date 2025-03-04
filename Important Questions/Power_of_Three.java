d57355ac3d9c9a6e48263f8d6f38c7f851464a9e
ayushman12baghel <ayushman12baghel@gmail.com>
2025-03-04 17:26:44 +0530

    Added Solution of Tabulation Approach

--- Important Questions/Power_of_Three.java
+++ Important Questions/Power_of_Three.java
@@ -1,5 +1,6 @@
 public class Power_of_Three {
 
+    // Approach 1 Using Recursion
     public static boolean isPowerThree(int n) {
         if (n == 1) {
             return true;
@@ -12,9 +13,23 @@ public class Power_of_Three {
         return isPowerThree(n / 3);
     }
 
+    // Approach 2 Using Tabulation
+    public static boolean isPowerThree2(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+
     public static void main(String args[]) {
         int n = 27;
 
         System.out.println(isPowerThree(n));
+        System.out.println(isPowerThree2(n));
     }
 }
