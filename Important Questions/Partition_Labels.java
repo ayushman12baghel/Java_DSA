8a3c4091fd1df657852efa38e5d0d781d932d6b0
ayushman12baghel <ayushman12baghel@gmail.com>
2025-03-30 15:02:49 +0530

    763. Partition Labels

---
+++ Important Questions/Partition_Labels.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Partition_Labels {
+
+    public static List<Integer> partitionLabels(String str) {
+        List<Integer> ans = new ArrayList<>();
+        int lastIndex[] = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            lastIndex[str.charAt(i) - 'a'] = i;
+        }
+
+        int i = 0;
+        while (i < str.length()) {
+            int end = lastIndex[str.charAt(i) - 'a'];
+            int j = i;
+
+            while (j < end) {
+                end = Math.max(end, lastIndex[str.charAt(j) - 'a']);
+                j++;
+            }
+
+            ans.add(j - i + 1);
+            i = j + 1;
+        }
+
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        String str = "ababcbacadefegdehijhklij";
+
+        System.out.println(partitionLabels(str));
+    }
+}
