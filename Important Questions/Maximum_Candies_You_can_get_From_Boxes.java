aac5ddd9b642a5c06700b31c569311ae63cf2903
ayushman12baghel <ayushman12baghel@gmail.com>
2025-06-04 00:28:44 +0530

    1298. Maximum Candies You Can Get from Boxes

--- Important Questions/Maximum_Candies_You_can_get_From_Boxes.java
+++ Important Questions/Maximum_Candies_You_can_get_From_Boxes.java
@@ -1,7 +1,9 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class Maximum_Candies_You_can_get_From_Boxes {
 
+    // Approach 1 Using DFS O(n)
     public static int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes,
             int[] initialBoxes) {
 
@@ -45,11 +47,60 @@ public class Maximum_Candies_You_can_get_From_Boxes {
         return candiesCollected;
     }
 
+    // Approach 2 Using BFS O(n)
+    public static int maxCandies2(int[] status, int[] candies, int[][] keys, int[][] containedBoxes,
+            int[] initialBoxes) {
+
+        int candiesCollected = 0;
+        boolean visited[] = new boolean[status.length];
+        Set<Integer> foundBoxes = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int box : initialBoxes) {
+            foundBoxes.add(box);
+
+            if (status[box] == 1) {
+                queue.offer(box);
+                visited[box] = true;
+                candiesCollected += candies[box];
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int box = queue.poll();
+
+            for (int insideBox : containedBoxes[box]) {
+                foundBoxes.add(insideBox);
+
+                if (status[insideBox] == 1 && !visited[insideBox]) {
+                    queue.offer(insideBox);
+                    visited[insideBox] = true;
+                    candiesCollected += candies[insideBox];
+                }
+            }
+
+            for (int key : keys[box]) {
+                status[key] = 1;
+
+                if (foundBoxes.contains(key) && !visited[key]) {
+                    queue.offer(key);
+                    visited[key] = true;
+                    candiesCollected += candies[key];
+                }
+            }
+        }
+
+        return candiesCollected;
+    }
+
     public static void main(String args[]) {
         int status[] = { 1, 0, 1, 0 };
         int candies[] = { 7, 5, 4, 100 };
         int keys[][] = { {}, {}, { 1 }, {} };
         int containedBoxes[][] = { { 1, 2 }, { 3 }, {}, {} };
         int initialBoxes[] = { 0 };
+
+        System.out.println(maxCandies(status, candies, keys, containedBoxes, initialBoxes));
+        System.out.println(maxCandies2(status, candies, keys, containedBoxes, initialBoxes));
     }
 }
