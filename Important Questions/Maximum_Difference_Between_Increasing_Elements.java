85ab59f317045724fcb2db96cee15f7c8441e89d
ayushman12baghel <ayushman12baghel@gmail.com>
2025-06-17 01:19:01 +0530

    2016. Maximum Difference Between Increasing Elements

--- Important Questions/Maximum_Difference_Between_Increasing_Elements.java
+++ Important Questions/Maximum_Difference_Between_Increasing_Elements.java
@@ -1,5 +1,6 @@
 public class Maximum_Difference_Between_Increasing_Elements {
 
+    // Approach 1
     public static int maximumDifference(int[] nums) {
         int ans = -1;
 
@@ -14,8 +15,25 @@ public class Maximum_Difference_Between_Increasing_Elements {
         return ans;
     }
 
+    // Appproach 2
+    public static int maximumDifference2(int[] nums) {
+        int ans = -1;
+        int min = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > min) {
+                ans = Math.max(ans, nums[i] - min);
+            } else {
+                min = nums[i];
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String args[]) {
         int nums[] = { 7, 1, 5, 4 };
         System.out.println(maximumDifference(nums));
+        System.out.println(maximumDifference2(nums));
     }
 }
