29187177527528e9d7b1bbcceaf66ce84275de35
ayushman12baghel <ayushman12baghel@gmail.com>
2025-06-27 19:34:56 +0530

    5. Longest Palindromic Substring

---
+++ Revisiting Concepts/DP/DP on Strings/Longest_Palindromic_Substring.java
@@ -0,0 +1,105 @@
+import java.util.*;
+
+public class Longest_Palindromic_Substring {
+
+    // Approach 1 Brute Force Memoisation O(n^3)
+    public static String longestPalindrome(String s) {
+        int n = s.length();
+        String ans = "";
+        Boolean dp[][] = new Boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(s, i, j, dp) && j - i + 1 > ans.length()) {
+                    ans = s.substring(i, j + 1);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static boolean isPalindrome(String str, int left, int right, Boolean dp[][]) {
+        int i = left;
+        int j = right;
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return dp[i][j] = false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return dp[i][j] = true;
+    }
+
+    // Approach 2 Using BluePrint Tabulation O(n^2)
+    public static String longestPalindrome2(String s) {
+        int n = s.length();
+
+        String ans = "";
+        boolean dp[][] = new boolean[n + 1][n + 1];
+
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L - 1 < n; i++) {
+                int j = i + L - 1;
+                if (i == j) {
+                    dp[i][j] = true;
+                } else if (i + 1 == j) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) ? true : false);
+                } else {
+                    if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                    }
+                }
+
+                if (dp[i][j] && ans.length() < j - i + 1) {
+                    ans = s.substring(i, j + 1);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    // Approach 3 Using Expand From Center O(n^2)
+    public static String longestPalindrome3(String s) {
+        int n = s.length();
+        String ans = "";
+
+        for (int i = 0; i < n; i++) {
+            String odd = expand(s, i, i);
+            String even = expand(s, i, i + 1);
+
+            if (odd.length() > ans.length()) {
+                ans = odd;
+            }
+            if (even.length() > ans.length()) {
+                ans = even;
+            }
+        }
+
+        return ans;
+    }
+
+    public static String expand(String str, int left, int right) {
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return str.substring(left + 1, right);
+    }
+
+    public static void main(String args[]) {
+        String s = "babad";
+
+        System.out.println(longestPalindrome(s));
+        System.out.println(longestPalindrome2(s));
+        System.out.println(longestPalindrome3(s));
+    }
+}
