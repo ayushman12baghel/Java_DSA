573c5f4267f4a2516e62da04b25b0d22024ddba1
ayushman12baghel <ayushman12baghel@gmail.com>
2025-07-03 18:14:03 +0530

    2024. Maximize the Confusion of an Exam

--- Important Questions/Maximise_the_Confusion_of_Exam.java
+++ Important Questions/Maximise_the_Confusion_of_Exam.java
@@ -48,10 +48,37 @@ public class Maximise_the_Confusion_of_Exam {
         return maxLength;
     }
 
+    // Approach 2 Using Single Pass SlidingWindow
+    public static int maxConsecutiveAnswers2(String str, int k) {
+        int n = str.length();
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('T', 0);
+        map.put('F', 0);
+
+        while (j < n) {
+            map.put(str.charAt(j), map.get(str.charAt(j)) + 1);
+
+            while (Math.min(map.get('T'), map.get('F')) > k) {
+                map.put(str.charAt(i), map.get(str.charAt(i)) - 1);
+                i++;
+            }
+
+            maxLength = Math.max(maxLength, j - i + 1);
+
+            j++;
+        }
+
+        return maxLength;
+    }
+
     public static void main(String[] args) {
         String ansKey = "TTFTTFTT";
         int k = 1;
 
         System.out.println(maxConsecutiveAnswers(ansKey, k));
+        System.out.println(maxConsecutiveAnswers2(ansKey, k));
     }
 }
