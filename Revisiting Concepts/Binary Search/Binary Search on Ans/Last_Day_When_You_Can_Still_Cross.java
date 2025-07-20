4120519fe90afcd42765c9e6c23aa6de7f591e75
ayushman12baghel <ayushman12baghel@gmail.com>
2025-07-21 03:00:48 +0530

    1970. Last Day Where You Can Still Cross

--- Revisiting Concepts/Binary Search/Binary Search on Ans/Last_Day_When_You_Can_Still_Cross.java
+++ Revisiting Concepts/Binary Search/Binary Search on Ans/Last_Day_When_You_Can_Still_Cross.java
@@ -1,7 +1,9 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class Last_Day_When_You_Can_Still_Cross {
 
+    // Approach1 Using BFS and Binary Search
     public static int latestDayToCross(int row, int col, int[][] cells) {
         int left = 0;
         int right = cells.length - 1;
@@ -72,10 +74,112 @@ public class Last_Day_When_You_Can_Still_Cross {
         return false;
     }
 
+    // Approach 2 Using DSU and Binary Search
+    static class DSU {
+        int[] parent;
+        int[] size;
+
+        public DSU(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            int px = find(x);
+            int py = find(y);
+            if (px == py)
+                return;
+
+            if (size[px] < size[py]) {
+                parent[px] = py;
+                size[py] += size[px];
+            } else {
+                parent[py] = px;
+                size[px] += size[py];
+            }
+        }
+    }
+
+    public static int latestDayToCross2(int row, int col, int[][] cells) {
+        int left = 0;
+        int right = cells.length - 1;
+        int lastDay = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canCross2(row, col, cells, mid)) {
+                lastDay = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return lastDay + 1;
+    }
+
+    public static boolean canCross2(int row, int col, int[][] cells, int day) {
+        int[][] grid = new int[row][col];
+        int total = row * col;
+        int topVirtual = total;
+        int bottomVirtual = total + 1;
+
+        DSU dsu = new DSU(total + 2);
+
+        for (int i = 0; i <= day; i++) {
+            int r = cells[i][0] - 1;
+            int c = cells[i][1] - 1;
+            grid[r][c] = 1;
+        }
+
+        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (grid[r][c] == 1)
+                    continue;
+
+                int currId = r * col + c;
+
+                if (r == 0) {
+                    dsu.union(currId, topVirtual);
+                }
+
+                if (r == row - 1) {
+                    dsu.union(currId, bottomVirtual);
+                }
+
+                for (int[] d : dirs) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+                    if (nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == 0) {
+                        int nextId = nr * col + nc;
+                        dsu.union(currId, nextId);
+                    }
+                }
+            }
+        }
+
+        // check if top is connected to bottom
+        return dsu.find(topVirtual) == dsu.find(bottomVirtual);
+    }
+
     public static void main(String[] args) {
         int row = 2, col = 2;
         int cells[][] = { { 1, 1 }, { 2, 1 }, { 1, 2 }, { 2, 2 } };
 
         System.out.println(latestDayToCross(row, col, cells));
+        System.err.println(latestDayToCross2(row, col, cells));
     }
 }
