61a1b36c557bf1362f4d43bba95ba7ada221ea3a
ayushman12baghel <ayushman12baghel@gmail.com>
2025-08-21 12:59:41 +0530

    222. Count Complete Tree Nodes

---
+++ Revisiting Concepts/Binary Tree/Count_Complete_Nodes.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
