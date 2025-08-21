ec4462713e073896a174e466508cf80cb599f71b
ayushman12baghel <ayushman12baghel@gmail.com>
2025-08-21 15:11:54 +0530

    222. Count Complete Tree Nodes

--- Revisiting Concepts/Binary Tree/Count_Complete_Nodes.java
+++ Revisiting Concepts/Binary Tree/Count_Complete_Nodes.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+//Basic Approach O(n)
 class Solution {
     public int countNodes(TreeNode root) {
         if (root == null) {
@@ -7,5 +8,39 @@ class Solution {
         }
 
         return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
+
+// Required Optimised Approach O(logn*logn)
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = leftHeight(root);
+        int rightHeight = rightHeight(root);
+
+        if (leftHeight == rightHeight) {
+            return (1 << leftHeight) - 1;
+        }
+
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+
+    public int leftHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + leftHeight(root.left);
+    }
+
+    public int rightHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + rightHeight(root.right);
     }
 }
