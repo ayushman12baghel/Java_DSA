b39b6ef0c2f74c3b05244b8ddbf8c7e5e13d7663
ayushman12baghel <ayushman12baghel@gmail.com>
2025-08-23 18:24:30 +0530

    525. Contiguous Array

--- Important Questions/Contagious_Array.java
+++ Important Questions/Contagious_Array.java
@@ -2,6 +2,28 @@ import java.util.*;
 
 public class Contagious_Array {
 
+    // THis looks easy to understand
+    public static int findMaxLength3(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += (nums[i] == 0 ? -1 : 1);
+
+            if (sum == 0) {
+                maxLength = i + 1;
+            } else if (map.containsKey(sum)) {
+                maxLength = Math.max(maxLength, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return maxLength;
+    }
+
     // Approach 1 With extra space
     public static int findMaxLength(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
