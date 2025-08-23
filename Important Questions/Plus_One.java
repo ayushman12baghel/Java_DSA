3f7812db2b28e35249746232b89b989e456bdc79
ayushman12baghel <ayushman12baghel@gmail.com>
2025-08-23 22:23:47 +0530

    66. Plus One

--- Important Questions/Plus_One.java
+++ Important Questions/Plus_One.java
@@ -1,16 +1,61 @@
 public class Plus_One {
 
-    public static int[] plusOne(int arr[]) {
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i] < 9) {
-                arr[i]++;
-                return arr;
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = digits[i];
+
+            if (digit < 9) {
+                digits[i] = digit + 1;
+
+                return digits;
+            } else {
+                digits[i] = 0;
             }
-            arr[i] = 0;
         }
-        arr = new int[arr.length + 1];
-        arr[0] = 1;
-        return arr;
+
+        int newNumber[] = new int[n + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+
+    // If the array has starting zeros
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = digits[i];
+
+            if (digit < 9) {
+                digits[i] += 1;
+
+                int firstIndex = 0;
+                while (firstIndex < n && digits[firstIndex] == 0) {
+                    firstIndex++;
+                }
+
+                if (firstIndex == 0) {
+                    return digits;
+                }
+
+                int ans[] = new int[n - firstIndex];
+
+                for (int j = 0; j < ans.length; j++) {
+                    ans[j] = digits[j + firstIndex];
+                }
+
+                return ans;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        int newNumber[] = new int[n + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
     }
 
     public static void main(String args[]) {
