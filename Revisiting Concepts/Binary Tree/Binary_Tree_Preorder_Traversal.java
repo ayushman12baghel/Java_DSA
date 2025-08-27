342282ad52b5ef2e2125b679cfd94416b21d142d
ayushman12baghel <ayushman12baghel@gmail.com>
2025-08-27 14:54:52 +0530

    144. Binary Tree Preorder Traversal

---
+++ Revisiting Concepts/Binary Tree/Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preorder(root, ans);
+
+        return ans;
+    }
+
+    public void preorder(TreeNode root, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+
+        ans.add(root.val);
+        preorder(root.left, ans);
+        preorder(root.right, ans);
+    }
+}
