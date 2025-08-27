c28e90c54751ca629177f7894cbcd1b92903e5db
ayushman12baghel <ayushman12baghel@gmail.com>
2025-08-27 14:59:44 +0530

    100. Same Tree

---
+++ Revisiting Concepts/Binary Tree/Same_Tree.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
