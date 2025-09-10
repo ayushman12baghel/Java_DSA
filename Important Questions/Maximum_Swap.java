0238838edb1c8fa638e84f746aff966bbc729e02
ayushman12baghel <ayushman12baghel@gmail.com>
2025-09-10 13:33:04 +0530

    70. Maximum Swap

--- Important Questions/Maximum_Swap.java
+++ Important Questions/Maximum_Swap.java
@@ -24,8 +24,37 @@ public class Maximum_Swap {
         return num;
     }
 
+    // Another Way
+    public static int maximumSwap2(int num) {
+        char digits[] = String.valueOf(num).toCharArray();
+        int n = digits.length;
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = n - 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (digits[i] > digits[rightMax[i + 1]]) {
+                rightMax[i] = i;
+            } else {
+                rightMax[i] = rightMax[i + 1];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (digits[i] < digits[rightMax[i]]) {
+                char temp = digits[i];
+                digits[i] = digits[rightMax[i]];
+                digits[rightMax[i]] = temp;
+
+                return Integer.parseInt(new String(digits));
+            }
+        }
+
+        return num;
+    }
+
     public static void main(String args[]) {
         int num = 2736;
         System.out.println(maximumSwap(num));
+        System.out.println(maximumSwap2(num));
     }
 }
