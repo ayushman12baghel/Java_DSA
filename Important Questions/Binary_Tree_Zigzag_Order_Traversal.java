bc8d0677ad9b4a0ec21164c16b04390da3dcff2e
ayushman12baghel <ayushman12baghel@gmail.com>
2025-09-10 15:54:14 +0530

    103. Binary Tree Zigzag Level Order Traversal

---
+++ Important Questions/Binary_Tree_Zigzag_Order_Traversal.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                temp.add(current.val);
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+            if (!leftToRight) {
+                Collections.reverse(temp);
+            }
+
+            result.add(temp);
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+    }
+}
