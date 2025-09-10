159fc981a1a0584f5988b6a41b0de163229d6e1b
ayushman12baghel <ayushman12baghel@gmail.com>
2025-09-10 16:00:51 +0530

    7. Remove Element

---
+++ Important Questions/Remove_Element.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
+            }
+        }
+
+        return i;
+    }
+}
