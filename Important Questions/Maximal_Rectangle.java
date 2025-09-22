5ce676bdd6765bfac698fa875a045adece28239b
ayushman12baghel <ayushman12baghel@gmail.com>
2025-09-22 11:21:18 +0530

    85. Maximal Rectangle

--- Important Questions/Maximal_Rectangle.java
+++ Important Questions/Maximal_Rectangle.java
@@ -107,3 +107,60 @@ class Solution {
         return ans;
     }
 }
+
+// Combining nse and pse in single loop
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int heights[] = new int[m];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                heights[j] = (matrix[i][j] == '1' ? heights[j] + 1 : 0);
+            }
+
+            ans = Math.max(ans, getMaxArea(heights));
+        }
+
+        return ans;
+    }
+
+    public int getMaxArea(int heights[]) {
+        int n = heights.length;
+
+        int pse[] = new int[n];
+        int nse[] = new int[n];
+        Stack<Integer> stackP = new Stack<>();
+        Stack<Integer> stackN = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stackP.isEmpty() && heights[stackP.peek()] >= heights[i]) {
+                stackP.pop();
+            }
+
+            while (!stackN.isEmpty() && heights[stackN.peek()] >= heights[n - i - 1]) {
+                stackN.pop();
+            }
+
+            pse[i] = (stackP.isEmpty() ? -1 : stackP.peek());
+            nse[n - i - 1] = (stackN.isEmpty() ? n : stackN.peek());
+
+            stackP.push(i);
+            stackN.push(n - i - 1);
+        }
+
+        int maxArea = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            int width = (nse[i] - pse[i] - 1);
+            int area = (width * heights[i]);
+
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+}
