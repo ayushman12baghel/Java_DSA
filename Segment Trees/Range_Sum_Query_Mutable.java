d156e60cd83c6baf3c2c7e416d6cd08794379870
ayushman12baghel <ayushman12baghel@gmail.com>
2025-09-23 14:15:10 +0530

    307. Range Sum Query - Mutable

--- Segment Trees/Range_Sum_Query_Mutable.java
+++ Segment Trees/Range_Sum_Query_Mutable.java
@@ -60,9 +60,82 @@ class NumArray {
     }
 }
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * obj.update(index,val);
- * int param_2 = obj.sumRange(left,right);
- */
+// Making SegmentTree to another class
+class SegmentTree {
+    int n;
+    int tree[];
+
+    public SegmentTree(int nums[]) {
+        n = nums.length;
+        tree = new int[4 * n];
+
+        buildST(nums, 0, 0, n - 1);
+    }
+
+    public int buildST(int nums[], int i, int start, int end) {
+        if (start == end) {
+            tree[i] = nums[start];
+            return tree[i];
+        }
+
+        int mid = start + (end - start) / 2;
+        int left = buildST(nums, 2 * i + 1, start, mid);
+        int right = buildST(nums, 2 * i + 2, mid + 1, end);
+
+        return tree[i] = left + right;
+    }
+
+    public void update(int index, int val) {
+        updateST(0, 0, n - 1, index, val);
+    }
+
+    public void updateST(int i, int start, int end, int index, int val) {
+        if (start == end) {
+            tree[i] = val;
+            return;
+        }
+
+        int mid = start + (end - start) / 2;
+        if (index <= mid) {
+            updateST(2 * i + 1, start, mid, index, val);
+        } else {
+            updateST(2 * i + 2, mid + 1, end, index, val);
+        }
+
+        tree[i] = tree[2 * i + 1] + tree[2 * i + 2];
+    }
+
+    public int query(int left, int right) {
+        return queryST(0, 0, n - 1, left, right);
+    }
+
+    public int queryST(int i, int start, int end, int left, int right) {
+        if (start > right || left > end) {
+            return 0;
+        } else if (left <= start && end <= right) {
+            return tree[i];
+        }
+
+        int mid = start + (end - start) / 2;
+        int leftSum = queryST(2 * i + 1, start, mid, left, right);
+        int rightSum = queryST(2 * i + 2, mid + 1, end, left, right);
+
+        return leftSum + rightSum;
+    }
+}
+
+class NumArray {
+    SegmentTree st;
+
+    public NumArray(int[] nums) {
+        st = new SegmentTree(nums);
+    }
+
+    public void update(int index, int val) {
+        st.update(index, val);
+    }
+
+    public int sumRange(int left, int right) {
+        return st.query(left, right);
+    }
+}
