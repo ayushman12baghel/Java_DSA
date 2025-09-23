3cc47a4d285bb314b830e0ba35e6861a122f2d95
ayushman12baghel <ayushman12baghel@gmail.com>
2025-09-23 14:32:19 +0530

    303. Range Sum Query - Immutable

--- Important Questions/Range_Sum_Query_Immutable.java
+++ Important Questions/Range_Sum_Query_Immutable.java
@@ -39,3 +39,65 @@ class NumArray {
         return temp[right] - temp[left - 1];
     }
 }
+
+// Using Segment Trees
+class SegmentTree {
+    int tree[];
+    int n;
+
+    public SegmentTree(int nums[]) {
+        n = nums.length;
+        tree = new int[4 * n];
+
+        buildST(nums, 0, 0, n - 1);
+    }
+
+    public int buildST(int nums[], int i, int start, int end) {
+        if (start == end) {
+            tree[i] = nums[start];
+            return tree[i];
+        }
+
+        int mid = start + (end - start) / 2;
+        buildST(nums, 2 * i + 1, start, mid);
+        buildST(nums, 2 * i + 2, mid + 1, end);
+
+        return tree[i] = tree[2 * i + 1] + tree[2 * i + 2];
+    }
+
+    public int query(int left, int right) {
+        return queryST(0, 0, n - 1, left, right);
+    }
+
+    public int queryST(int i, int start, int end, int left, int right) {
+        if (left > end || start > right) {
+            return 0;
+        } else if (start >= left && right >= end) {
+            return tree[i];
+        }
+
+        int mid = start + (end - start) / 2;
+        int leftSum = queryST(2 * i + 1, start, mid, left, right);
+        int rightSum = queryST(2 * i + 2, mid + 1, end, left, right);
+
+        return leftSum + rightSum;
+    }
+}
+
+class NumArray {
+    SegmentTree st;
+
+    public NumArray(int[] nums) {
+        st = new SegmentTree(nums);
+    }
+
+    public int sumRange(int left, int right) {
+        return st.query(left, right);
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
