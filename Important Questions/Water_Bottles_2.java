0509a17993e14cad4760e4ecc79ac43f99dac1f8
ayushman12baghel <ayushman12baghel@gmail.com>
2025-10-02 09:33:48 +0530

    3100. Water Bottles II

---
+++ Important Questions/Water_Bottles_2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = numBottles;
+
+        while (numBottles >= numExchange) {
+            numBottles -= numExchange;
+            numExchange++;
+            ans++;
+            numBottles++;
+        }
+
+        return ans;
+    }
+}
