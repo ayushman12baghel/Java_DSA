2cac265ca8fbd199071c3591c282a01ffccedb11
ayushman12baghel <ayushman12baghel@gmail.com>
2025-11-04 14:45:18 +0530

    3318. Find X-Sum of All K-Long Subarrays I

--- Important Questions/Find_X_Sum_of_All_K_Long_Subarrays_I.java
+++ Important Questions/Find_X_Sum_of_All_K_Long_Subarrays_I.java
@@ -58,3 +58,167 @@ public class Find_X_Sum_of_All_K_Long_Subarrays_I {
         }
     }
 }
+
+// Approach 2 Using HashMap and PriorityQueue O(n*klogk)
+class Pair implements Comparable<Pair> {
+    int key;
+    int value;
+
+    public Pair(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(Pair p) {
+        if (p.value == this.value) {
+            return p.key - this.key;
+        }
+
+        return p.value - this.value;
+    }
+}
+
+class Solution {
+    public int[] findXSum(int[] nums, int k, int x) {
+        int n = nums.length;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans[] = new int[n - k + 1];
+        for (int i = 0; i < k; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        ans[0] = getXSum(map, x);
+
+        for (int i = k; i < n; i++) {
+            int out = nums[i - k];
+            map.put(out, map.get(out) - 1);
+            if (map.get(out) == 0) {
+                map.remove(out);
+            }
+
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            ans[i - k + 1] = getXSum(map, x);
+        }
+
+        return ans;
+    }
+
+    public int getXSum(Map<Integer, Integer> map, int x) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.offer(new Pair(entry.getKey(), entry.getValue()));
+        }
+
+        int sum = 0;
+
+        while (!pq.isEmpty() && x-- > 0) {
+            Pair top = pq.poll();
+            sum += (top.key * top.value);
+        }
+
+        return sum;
+    }
+}
+
+// Approach 3 Using TreeMap O(nlogk)
+class Pair implements Comparable<Pair> {
+    int key;
+    int value;
+
+    public Pair(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(Pair p) {
+        if (p.value == this.value) {
+            return p.key - this.key;
+        }
+
+        return p.value - this.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof Pair)) {
+            return false;
+        }
+
+        Pair p = (Pair) o;
+
+        return key == p.key && value == p.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+}
+
+class Solution {
+    public int[] findXSum(int[] nums, int k, int x) {
+        int n = nums.length;
+
+        int ans[] = new int[n - k + 1];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        TreeMap<Pair, Integer> tree = new TreeMap<>();
+
+        for (int i = 0; i < k; i++) {
+            int old = map.getOrDefault(nums[i], 0);
+            if (old > 0) {
+                tree.remove(new Pair(nums[i], old));
+            }
+
+            map.put(nums[i], old + 1);
+            tree.put(new Pair(nums[i], old + 1), 1);
+        }
+
+        ans[0] = getXSum(tree, x);
+
+        for (int i = k; i < n; i++) {
+            int old = map.get(nums[i - k]);
+            tree.remove(new Pair(nums[i - k], old));
+            if (old == 1) {
+                map.remove(nums[i - k]);
+            } else {
+                map.put(nums[i - k], old - 1);
+                tree.put(new Pair(nums[i - k], old - 1), 1);
+            }
+
+            old = map.getOrDefault(nums[i], 0);
+            if (old > 0) {
+                tree.remove(new Pair(nums[i], old));
+            }
+
+            map.put(nums[i], old + 1);
+            tree.put(new Pair(nums[i], old + 1), 1);
+
+            ans[i - k + 1] = getXSum(tree, x);
+        }
+
+        return ans;
+    }
+
+    public int getXSum(TreeMap<Pair, Integer> tree, int x) {
+        int sum = 0;
+        int count = 0;
+
+        for (Pair p : tree.keySet()) {
+            if (count == x) {
+                break;
+            }
+
+            sum += p.key * p.value;
+            count++;
+        }
+
+        return sum;
+    }
+}
