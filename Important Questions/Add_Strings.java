e253408720b542816fd75676e7ee1008fa1df975
ayushman12baghel <ayushman12baghel@gmail.com>
2025-11-10 14:33:35 +0530

    415. Add Strings

---
+++ Important Questions/Add_Strings.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += (int) (num1.charAt(i) - '0');
+            }
+            if (j >= 0) {
+                sum += (int) (num2.charAt(j) - '0');
+            }
+            sb.append(sum % 10);
+            carry = sum / 10;
+
+            i--;
+            j--;
+        }
+
+        return sb.reverse().toString();
+    }
+}
