bb1fff3d5d5292f9406884c0bd1c422d51def55c
ayushman12baghel <ayushman12baghel@gmail.com>
2025-12-01 10:06:58 +0530

    134. Gas Station

--- Important Questions/Gas_Station.java
+++ Important Questions/Gas_Station.java
@@ -1,31 +1,66 @@
 import java.util.*;
 
+//Brute Force O(n^2)
 class Solution {
-    public int startStation(int[] gas, int[] cost) {
-        int n=gas.length;
-        int startIndex=0;
-        int currGas=0;
-        
-        for(int i=0;i<n;i++){
-            currGas=currGas+gas[i]-cost[i];
-            
-            if(currGas<0){
-                currGas=0;
-                startIndex=i+1;
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+
+        for (int i = 0; i < n; i++) {
+            int currentGas = gas[i];
+            int currentCost = cost[i];
+
+            if (currentCost > currentGas) {
+                continue;
+            }
+
+            int j = i + 1;
+            j %= n;
+
+            while (j != i) {
+                currentGas += gas[j];
+                currentCost += cost[j];
+
+                if (currentCost > currentGas) {
+                    break;
+                }
+                j = (j + 1) % n;
+            }
+
+            if (j == i) {
+                return i;
             }
         }
-        
-        currGas=0;
-        
-        for(int i=0;i<n;i++){
-            int index=(i+startIndex)%n;
-            currGas=currGas+gas[index]-cost[index];
-            
-            if(currGas<0){
-                return -1;
+
+        return -1;
+    }
+}
+
+// Approach 2 Greedy O(n)
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += (gas[i] - cost[i]);
+        }
+
+        if (total < 0) {
+            return -1;
+        }
+
+        total = 0;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            total += (gas[i] - cost[i]);
+
+            if (total < 0) {
+                total = 0;
+                result = i + 1;
             }
         }
-        
-        return startIndex;
+
+        return result;
     }
 }
