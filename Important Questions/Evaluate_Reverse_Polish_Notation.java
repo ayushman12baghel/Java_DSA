c6105aed70f7e05d96137d0c5f932b9e479db26b
ayushman12baghel <ayushman12baghel@gmail.com>
2025-12-05 15:37:25 +0530

    150. Evaluate Reverse Polish Notation

---
+++ Important Questions/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+//Approach Using Stack O(n)
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (token.equals("-")) {
+                int first = stack.pop();
+                int second = stack.pop();
+                stack.push(second - first);
+            } else if (token.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (token.equals("/")) {
+                int first = stack.pop();
+                int second = stack.pop();
+                stack.push(second / first);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
