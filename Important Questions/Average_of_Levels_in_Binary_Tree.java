683aa1cc433943e74b9953d375138e6716cd5ff9
ayushman12baghel <ayushman12baghel@gmail.com>
2025-12-10 15:52:43 +0530

    637. Average of Levels in Binary Tree

---
+++ Important Questions/Average_of_Levels_in_Binary_Tree.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+//Approach Using BFS O(n)
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                sum += current.val;
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+            double temp = (sum / size);
+            ans.add(temp);
+        }
+
+        return ans;
+    }
+}
