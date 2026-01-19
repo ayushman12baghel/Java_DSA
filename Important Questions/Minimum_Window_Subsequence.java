36e99955b810f2eba7386303f33cbc1bbb938777
ayushman12baghel <ayushman12baghel@gmail.com>
2026-01-19 20:24:44 +0530

    Minimum Window Subsequence -> GFG

---
+++ Important Questions/Minimum_Window_Subsequence.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+// Approach Using Two Pointers O(n*n) on average O(n)
+class Solution {
+    public String minWindow(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int i = 0;
+        int j = 0;
+        int minLength = Integer.MAX_VALUE;
+        String result = "";
+
+        while (i < n) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                j++;
+            }
+
+            if (j == m) {
+                int end = i;
+                j--;
+
+                while (j >= 0) {
+                    if (s2.charAt(j) == s1.charAt(i)) {
+                        j--;
+                    }
+
+                    i--;
+                }
+
+                i++;
+
+                int length = end - i + 1;
+                if (length < minLength) {
+                    minLength = length;
+                    result = s1.substring(i, end + 1);
+                }
+
+                j = 0;
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+}
