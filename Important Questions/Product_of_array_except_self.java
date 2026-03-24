6788d97a125393fbc56d21b5b45d69f4aff26b94
ayushman12baghel <ayushman12baghel@gmail.com>
2026-03-24 21:59:43 +0530

    238. Product of Array Except Self

--- Important Questions/Product_of_array_except_self.java
+++ Important Questions/Product_of_array_except_self.java
@@ -55,3 +55,25 @@ public class Product_of_array_except_self {
 
     }
 }
+
+// Approach 2 O(n)
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+
+        int ans[] = new int[n];
+        ans[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * right;
+            right *= nums[i];
+        }
+
+        return ans;
+    }
+}
