ade7a0096a07010d9aa2c713225c0db0a2ffbd1f
ayushman12baghel <ayushman12baghel@gmail.com>
2026-06-22 11:32:44 +0530

    116. Populating Next Right Pointers in Each Node

--- Important Questions/Populating_Next_Right_Pointers_in_Each_Node.java
+++ Important Questions/Populating_Next_Right_Pointers_in_Each_Node.java
@@ -31,3 +31,39 @@ class Solution {
         return root;
     }
 }
+
+// Approach 2 BFS
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Node> list = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
+                list.add(current);
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+            for (int i = 0; i < list.size() - 1; i++) {
+                list.get(i).next = list.get(i + 1);
+            }
+        }
+
+        return root;
+    }
+}
